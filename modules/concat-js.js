4d4839d905f93c443ee913752d4f8db3374becc6
yleichanok <e.leychenok@gmail.com>
2012-10-09 13:05:34 +0300

    Small enhancements in concat-js.js

--- modules/concat-js.js
+++ modules/concat-js.js
@@ -5,8 +5,8 @@
 * Usage:
 * 
 * var cjs = require('./concat-js');
-* cjs.process(['C:/work/js/1.js', 'C:/work/js/2.js'], 'C:/work/js/output.js');
-* cjs.processFolder('C:/work/js/', 'C:/work/js/output.js');
+* cjs.concat(['C:/work/js/1.js', 'C:/work/js/2.js'], 'C:/work/js/output.js');
+* cjs.concatFolder('C:/work/js/', 'C:/work/js/output.js');
 *
 */
 /*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, unused:true, curly:true, node:true, indent:4, maxerr:50, globalstrict:true, white:false */
@@ -18,12 +18,12 @@
 
     /**
     * Concatenates all the files specified into a single file.
-    * @param {Array} inputFiles Array of file paths to concatenate.
+    * @param {Array} files Array of file paths to concatenate.
     * @param {String} outputFilePath Path to save a result.
     */
-    exports.process = function(inputFiles, outputFilePath) {
+    exports.concat = function(files, outputFilePath) {
 
-        if (!inputFiles || !Array.isArray(inputFiles)) {
+        if (!files || !Array.isArray(files)) {
             throw new Error('Input files not specified.');
         }
 
@@ -33,8 +33,8 @@
 
         var outputFileContents = '';
 
-        for (var i = 0, l = inputFiles.length; i < l; i++) {
-            var fileContents = fs.readFileSync(inputFiles[i], 'utf8').toString();
+        for (var i = 0, l = files.length; i < l; i++) {
+            var fileContents = fs.readFileSync(files[i], 'utf8').toString();
             outputFileContents += fileContents;
         }
 
@@ -44,12 +44,12 @@
 
     /**
     * Concatenates all the files in the folder specified and inner folders into a single file.
-    * @param {String} inputPath Folder to look files at.
+    * @param {String} path Folder to look files at.
     * @param {String} outputFilePath Path to save a result.
     */
-    exports.processFolder = function(inputPath, outputFilePath) {
+    exports.concatFolder = function(path, outputFilePath) {
         
-        if (!inputPath || typeof inputPath !== 'string') {
+        if (!path || typeof path !== 'string') {
             throw new Error('Input path not specified');
         }
         
@@ -74,7 +74,7 @@
             }
         };
 
-        processFolder(inputPath);
+        processFolder(path);
 
         fs.writeFileSync(outputFilePath, outputFileContents, 'utf8');
 
